907b312bb2b7044eda799a82de3e76a5897d564c
amritchapai <chapaiamrit@gmail.com>
2023-06-28 19:31:47 +0545

    addition of pause and resume music

--- include/framework/sound.hpp
+++ include/framework/sound.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include "SDL_mixer.h"
 class Sound{
-    Mix_Music *run_music;
+    static Mix_Music *run_music;
     int loop;
     public:
-    Sound(char* path, int loop);
-    ~Sound();
+    static void PlayMusic(char *path, int loop);
+    static void PauseMusic();
+    static void ResumeMusic();
 };
--- src/framework/sound.cpp
+++ src/framework/sound.cpp
@@ -1,13 +1,32 @@
 #include "framework/sound.hpp"
-#include "stdio.h"
+#include<iostream>
 
+void Sound ::PlayMusic(char *path, int loop)
+{
+    if (Mix_OpenAudio(22020, AUDIO_S16SYS, 2, 4096) != 0) // initializes the audio device
+    {
+        printf("can't play the music");
+    }
+    run_music = Mix_LoadMUS(path); // creates the music object of the music in path;
+    Mix_PlayMusic(run_music, loop);
+}
 
-Sound :: Sound(char* path, int loop){
-            if(Mix_OpenAudio(22020, AUDIO_S16SYS, 2, 4096) != 0){  //initializes the audio device
-                printf("can,t play the music");
-            }
-            run_music = Mix_LoadMUS(path); // creates the music object of the music in path;
-            Mix_PlayMusic(run_music, loop);   
-} 
+void Sound :: PauseMusic()
+{
+    if(Mix_PlayingMusic)
+    {
+        Mix_PauseMusic();
+    }
+    else
+        printf("Music isn't playing");
+}
 
-Sound ::~Sound(){}
+void Sound :: ResumeMusic()
+{
+    if (Mix_PausedMusic)
+    {
+        Mix_ResumeMusic();
+    }
+    else
+        printf("Music isn't paused");
+}
