b2e54c65fad22bfd33b037b6e513f05c92508c9e
amritchapai <chapaiamrit@gmail.com>
2023-07-04 09:51:46 +0545

    singleton update

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
 #include "framework/time.hpp"
 #include "framework/draw.hpp"
-int main(int argc,  char* argv[]) {
+int main(int argc, char *argv[])
+{
 
-	(void) argc;
-    (void) argv;
+    (void)argc;
+    (void)argv;
 
-	std::cout << "Hello from main" << std::endl;
+    std::cout << "Hello from main" << std::endl;
 
-	Time *time = new Time();
-	
-	Draw* singleton = Draw::getInstance();
-	singleton->sInitializeSDL();
-	SDL_Rect srcRect;
-	SDL_Rect dstRect;
-	srcRect.w = 32;
-	srcRect.h = 32;
-	srcRect.x = 0;
-	srcRect.y = 0;
-	dstRect.w = 64;
-	dstRect.h = 64;
-	dstRect.x = 300;
-	dstRect.y = 400;
-	
-	bool isRunning = singleton->sCheckRunning();
-	while (isRunning)
-	{
-		singleton->sHandleEvents();
-		//singleton->sFullScreenDraw("../res/images/healthbar1.png", srcRect, dstRect);
-		singleton->sDrawTexture("../res/images/healthbar1.png", srcRect, dstRect);
-		isRunning = singleton->sCheckRunning();
-	}
-	singleton->DestroySDL();
-	time->hello();
+    Time *time = new Time();
 
-	return 0;
+    Draw &singleton = Draw::getInstance();
+
+    singleton.InitializeSDL();
+
+    SDL_Rect srcRect;
+    SDL_Rect dstRect;
+    srcRect.w = 32;
+    srcRect.h = 32;
+    srcRect.x = 0;
+    srcRect.y = 0;
+    dstRect.w = 64;
+    dstRect.h = 64;
+    dstRect.x = 500;
+    dstRect.y = 500;
+
+    bool isRunning = singleton.CheckRunning();
+    singleton.LoadTexture("../res/images/arrow_left.png");
+    while (isRunning)
+    {
+        singleton.HandleEvents();
+        // singleton->sFullScreenDraw("../res/images/healthbar1.png", srcRect, dstRect);
+        
+        singleton.DrawTexture( srcRect, dstRect);
+        isRunning = singleton.CheckRunning();
+    }
+    singleton.DestroySDL();
+    time->hello();
+    return 0;
 }
