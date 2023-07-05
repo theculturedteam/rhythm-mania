317ccfb5143149052495b6f9e53b2d447c7f0a35
FosRexx <jackgahire@gmail.com>
2023-07-05 18:41:27 +0545

    Input system initial

--- include/Message.hpp
+++ include/Message.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "framework/inputData.hpp"
-#include "framework/soundData.hpp"
+#include "data/inputData.hpp"
+#include "data/soundData.hpp"
 
 class Message
 {
