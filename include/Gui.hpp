6a126c9cc9e9066e91e57ed9ea9fa6915decf8f6
MilanRegmiGitH <milanregmi561@gmail.com>
2023-07-21 14:03:08 +0545

    bro bro gui bro

--- include/Gui.hpp
+++ include/Gui.hpp
@@ -2,17 +2,22 @@
 #include <SDL_ttf.h>
 #include <SDL.h>
 #include "draw.hpp"
+#include "GameObjects.hpp"
 struct Font{
     SDL_Texture* text;
     SDL_Rect position;
 };
 
 class Gui{
+    GameObject* textButton; 
     SDL_Surface* sText;
-    Draw& instance;
+    Font font1;
+    TTF_Font* textFont;
+    SDL_Color* 
+    uint16_t index;
+    Draw& instance = Draw::getInstance();
     public:
-    Gui();
     void setPosition(int x, int y, int w, int h);
-    void drawText();
+    GameObject* drawText();
 
 };
--- src/Gui.cpp
+++ src/Gui.cpp
@@ -1,18 +1,23 @@
 #include "Gui.hpp"
 
 Gui::Gui(){
-    Font font1;
-    instance = Draw::getInstance();
+    textButton = new GameObject("texture", "position", nullptr);
 }
 
-Gui::setPosition(int xint, int yint, int wint, int hint){
+void Gui::setPosition(int xint, int yint, int wint, int hint){
     font1.position.x = xint;
     font1.position.y = yint;
     font1.position.w = wint;
     font1.position.h = hint;
+    sText = TTF_RenderText_Blended();
 }
 
-Gui::drawText(){
-    text = Draw::ConvertTexture(sText);
-    
+GameObject* Gui::drawText(){
+
+    font1.text = instance.ConvertTexture(sText);
+    index = instance.addTexture(font1.text);
+
 }
+
+
+
