8e25c6c6bb93550d17173c37e0614d52c52e42f4
MilanRegmiGitH <milanregmi561@gmail.com>
2023-07-21 09:58:00 +0545

    gui

---
+++ include/Gui.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SDL_ttf.h>
+#include <SDL.h>
+
+struct{
+    SDL_Texture* text;
+    SDL_Rect position;
+};
+
+class Gui{
+    SDL_Surface* text;
+    public:
+    void setPosition(int x, int y, int w, int h);
+    
+};
