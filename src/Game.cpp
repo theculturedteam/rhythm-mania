522e1e430fdf0a31c32020df86bcfdbc700f5321
amritchapai <chapaiamrit@gmail.com>
2023-07-22 05:48:19 +0545

    modified audio text

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,23 +1,24 @@
 #include "Game.hpp"
 #include "GameLogic.hpp"
 
-Game :: Game()
-    :renderSystem(&msgBus, &gameObjects), inputSystem(&msgBus), gameLogic(&msgBus, &gameObjects, &isRunning), soundSystem(&msgBus)
+Game ::Game()
+    : renderSystem(&msgBus, &gameObjects), inputSystem(&msgBus), gameLogic(&msgBus, &gameObjects, &isRunning), soundSystem(&msgBus)
 {
     isRunning = true;
 }
 
+Game ::~Game()
+{
+}
 
-Game :: ~Game()
-{}
-
-void Game :: run()
+void Game ::run()
 {
     gameLogic.start();
     Time::sGetInstance().setPreviousTime();
-    while(isRunning)
+    while (isRunning)
     {
         Time::sGetInstance().calculateDeltaTime();
+        std::cout << "dt: " << Time::sGetInstance().getDeltaTime() << std::endl;
         inputSystem.update();
         renderSystem.update();
         soundSystem.update();
